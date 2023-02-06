88b2f6f5e468782ffb19bbfdf523aad121ecb70e
esantb <e.saint.bou@gmail.com>
2023-02-05 20:37:49 -0600

    holy moly she's looping

--- database.js
+++ database.js
@@ -1,50 +1,72 @@
-function showCustomerName(customerArray) {
-    for (let customer of customerArray) {
+console.log('database.js linked');
 
-        let customerDiv = document.createElement("div");
-        customerDiv.classList.add("customer")
-        let nameDiv = document.createElement("div");
-        nameDiv.classList.add("name");
-        nameDiv.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
-        customerDiv.appendChild(nameDiv);
-        container.appendChild(customerDiv);
+let directory = document.querySelector("#container");
 
-        // image
-        let imageDiv = document.createElement("img");
-        imageDiv.classList.add("image");
-        imageDiv.src = `${customer.picture.large}`;
-        customerDiv.appendChild(imageDiv);
+for (let customer of customers) {
+    customerHtml(customer)
+}
 
-        // email
-        let emailDiv = document.createElement("div");
-        emailDiv.classList.add("email");
-        emailDiv.innerText = `${customer.email}`;
-        customerDiv.appendChild(emailDiv);
-
-        // street
-        let streetDiv = document.createElement("div");
-        streetDiv.classList.add("street");
-        streetDiv.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
-        customerDiv.appendChild(streetDiv);
-
-        // state/zip
-        let stateDiv = document.createElement("div");
-        stateDiv.classList.add("state");
-        stateDiv.innerText = `${customer.city} ${customer.state} ${customer.postcode}`;
-        customerDiv.appendChild(stateDiv);
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 
-        // DOB
-        let birthdayDiv = document.createElement("div");
-        birthdayDiv.classlist.add("DOB");
-        birthdayDiv.innerText = `${customer.dob.date}`;
-        birthdayDiv.appendChild(birthdayDiv);
+function customerHtml(customer) {
+    let customerDiv = document.createElement("div");
+    customerDiv.classList.add("customer-box");
+
+    // image
+    let img = document.createElement("img");
+    img.classList.add("picture");
+    img.src = (`${customer.picture.thumbnail}`);
+    customerDiv.appendChild(img);
+    let picture = document.createElement("h1");
+    picture.appendChild(img);
+    customerDiv.appendChild(picture);
+    
+    // name
+    let nameDiv = document.createElement("h2");
+    nameDiv.classList.add("customerName");
+    let customerName = document.createTextNode(`${capitalizeFirstLetter(customer.name.title)} ${capitalizeFirstLetter(customer.name.first)} ${capitalizeFirstLetter(customer.name.last)}`)
+    nameDiv.appendChild(customerName);
+    customerDiv.appendChild(nameDiv);
+    directory.appendChild(customerDiv);
 
 
-        // customer since date 
-        let regDiv = document.createElement("div");
-        regDiv.classList.add("Customer since");
-        regDiv.innerText = `${customer.registered.date}`;
-        regDiv.appendChild(regDiv);
-    }
+    // email
+    let emailDiv = document.createElement("div");
+    emailDiv.classList.add("customerEmail");
+    let email = document.createTextNode(`${customer.email}`);
+    emailDiv.appendChild(email)
+    customerDiv.appendChild(emailDiv);
+
+    // street
+    let streetDiv = document.createElement("div");
+    streetDiv.classList.add("street");
+    let streetName = document.createTextNode(`${customer.location.street.number} ${customer.location.street.name}`);
+    streetDiv.appendChild(streetName);
+    customerDiv.appendChild(streetDiv);
+
+    // state/zip
+    let stateDiv = document.createElement("div");
+    stateDiv.classList.add("state");
+    let stateZip = document.createTextNode(`${customer.location.city}, ${customer.location.state} ${customer.location.postcode}`);
+    stateDiv.appendChild(stateZip);
+    customerDiv.appendChild(stateDiv);
+
+
+    // DOB
+    let dobDiv = document.createElement("div");
+    dobDiv.classList.add("DOB");
+    let txtDOB = document.createTextNode(`DOB: ${moment(customer.dob.date).format("MMM Do YYYY")}`)
+    dobDiv.appendChild(txtDOB);
+    customerDiv.appendChild(dobDiv);
+
+
+    // customer since date 
+    let regDiv = document.createElement("div");
+    regDiv.classList.add("CustomerSince");
+    let registration = document.createTextNode(`Customer since: ${moment(customer.registered.date).format("MMM Do YYYY")}`);
+    regDiv.appendChild(registration)
+    customerDiv.appendChild(regDiv);
 }
