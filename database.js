59689ff1b8c5640dfe3ead6a9263e36ada69c253
esantb <e.saint.bou@gmail.com>
2023-02-05 18:04:59 -0600

    all the customer divs

--- database.js
+++ database.js
@@ -3,33 +3,44 @@ let customerDiv = document.createElement("container");
 customerDiv.classList.add("customer")
 let nameDiv = document.createElement("div");
 nameDiv.classList.add("name");
-nameDiv.innerText = '${customer.name.title} ${customer.name.first} ${customer.name.last}';
+nameDiv.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
 customerDiv.appendChild(nameDiv);
 container.appendChild(customerDiv);
 
 // image
-
+let imageDive = document.createElement("img");
+imageDive.classList.add("image");
+imageDive.src = `${customer.picture.large}`;
+customerDiv.appendChild(imageDiv);
 
 // email
 let emailDiv = document.createElement("div");
 emailDiv.classList.add("email");
-emailDiv.innerText = '${customer.email}';
+emailDiv.innerText = `${customer.email}`;
 customerDiv.appendChild(emailDiv);
 
 // street
 let streetDiv = document.createElement("div");
 streetDiv.classList.add("street");
-streetDiv.innerText = '${customer.location.street.number} ${customer.location.street.name}';
+streetDiv.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
 customerDiv.appendChild(streetDiv);
 
 // state/zip
 let stateDiv = document.createElement("div");
 stateDiv.classList.add("state");
-stateDiv.innerText = '${customer.city} {customer.state} {customer.postcode}';
+stateDiv.innerText = `${customer.city} ${customer.state} ${customer.postcode}`;
 customerDiv.appendChild(stateDiv);
 
 
 // DOB
+let birthdayDiv = document.createElement("div");
+birthdayDiv.classlist.add("DOB");
+birthdayDiv.innerText = `${customer.dob.date}`;
+birthdayDiv.appendChild(birthdayDiv);
 
 
 // customer since date 
+let regDiv = document.createElement("div");
+regDiv.classList.add("Customer since");
+regDiv.innerText = `${customer.registered.date}`;
+regDiv.appendChild(regDiv);
